146a1b8aca5828878a65e632929e940a99d2fc63
lmigonzalez <lmigonzalezs@gmail.com>
2024-06-09 15:29:59 -0400

    new

--- src/components/HomePurchase.tsx
+++ src/components/HomePurchase.tsx
@@ -11,6 +11,7 @@ import {
 export const homePurchaseInitial = {
   values: {
     homePrice: "100,000",
+    homeValueAppreciation: "3",
     downPayment: "20",
     interestRate: "5",
     loanTerm: "30",
@@ -23,7 +24,6 @@ export const homePurchaseInitial = {
     HOAFee: "0",
     maintenanceCost: "0",
     maintenanceCostType: "$",
-    homeValueAppreciation: "0",
     sellClosingCost: "0",
     closing: "no",
   },
@@ -168,6 +168,57 @@ function HomePurchase() {
             <span className="absolute left-0 top-0 h-10 px-2 flex justify-center items-center text-gray-500 font-thin">
               $
             </span>
+          </div>
+        </div>
+        <div className="flex flex-col text-start">
+          <label>Annual Home Value Appreciation:</label>
+          <div className="flex flex-col md:flex-row gap-3 md:gap-5 justify-between items-center">
+            <div className="relative w-full md:w-2/3">
+              <input
+                name="homeValueAppreciation"
+                value={purchaseData.homeValueAppreciation}
+                onChange={formatterInputHandlingChangeWithValidation(
+                  handleChange,
+                  homePurchaseInitial.validations.homeValueAppreciation
+                )}
+                type="text"
+                className="h-10 px-2 rounded border w-full"
+              />
+              <span className="absolute right-0 top-0 h-10 px-2 flex justify-center items-center text-gray-500 font-thin">
+                % - year
+              </span>
+            </div>
+            <div className="w-full md:w-1/3 flex flex-col md:items-start">
+              <div className="">
+                {" "}
+                <p className="flex items-center text-[#9CA3AF]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-5 "
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  HELP
+                </p>
+                <button
+                  type="button"
+                  className="text-[#2ea6e9] text-sm"
+                  onClick={() => {
+                    setText(
+                      "This is the percentage amount that you anticipate your home's value will appreciate on average each year over the duration of your ownership of the property. <space> A good conservative benchmark is the average US annual inflation rate, which has been just over 3% annually for the last one hundred years."
+                    );
+                  }}
+                >
+                  Whats this?
+                </button>
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex flex-col text-start ">
@@ -442,57 +493,6 @@ function HomePurchase() {
               </button>{" "}
               - year
             </span>
-          </div>
-        </div>
-        <div className="flex flex-col text-start">
-          <label>Home Value Appreciation:</label>
-          <div className="flex flex-col md:flex-row gap-3 md:gap-5 justify-between items-center">
-            <div className="relative w-full md:w-2/3">
-              <input
-                name="homeValueAppreciation"
-                value={purchaseData.homeValueAppreciation}
-                onChange={formatterInputHandlingChangeWithValidation(
-                  handleChange,
-                  homePurchaseInitial.validations.homeValueAppreciation
-                )}
-                type="text"
-                className="h-10 px-2 rounded border w-full"
-              />
-              <span className="absolute right-0 top-0 h-10 px-2 flex justify-center items-center text-gray-500 font-thin">
-                % - year
-              </span>
-            </div>
-            <div className="w-full md:w-1/3 flex flex-col md:items-start">
-              <div className="">
-                {" "}
-                <p className="flex items-center text-[#9CA3AF]">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-5 "
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 0 1-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 0 1-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 0 1-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584ZM12 18a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  HELP
-                </p>
-                <button
-                  type="button"
-                  className="text-[#2ea6e9] text-sm"
-                  onClick={() => {
-                    setText(
-                      "This is the percentage amount that you anticipate your home's value will appreciate on average each year over the duration of your ownership of the property. <space> A good conservative benchmark is the average US annual inflation rate, which has been just over 3% annually for the last one hundred years."
-                    );
-                  }}
-                >
-                  Whats this?
-                </button>
-              </div>
-            </div>
           </div>
         </div>
         <div className="flex flex-col text-start md:pr-5">
