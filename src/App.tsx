1c1cd6a85a7b1180afa4f8de48dfba341b4a49e1
lmigonzalez <lmigonzalezs@gmail.com>
2024-06-16 19:56:05 -0400

    print

--- src/App.tsx
+++ src/App.tsx
@@ -110,16 +110,6 @@ function App() {
         isPrinting ? "w-[800px] px-4 min-h-screen text-center" : ""
       )}
     >
-      <button
-        onClick={handlePrint}
-        className={cn(
-          "hover:scale-95 transition-all",
-          isPrinting ? "hidden" : ""
-        )}
-      >
-        Print
-      </button>
-
       <div
         className={cn(
           "flex flex-col md:flex-row justify-between gap-10 bg-center bg-cover",
@@ -148,6 +138,33 @@ function App() {
           </div>
         </div>
       </div>
+      {result && (
+        <div className="flex justify-center mt-10">
+          <button
+            onClick={handlePrint}
+            className={cn(
+              "px-8 h-10 bg-[#2AB499] text-white flex justify-between items-center gap-3 hover:bg-[#66fbde] border hover:border-[#2AB499] hover:text-black transition-all",
+              isPrinting ? "hidden" : ""
+            )}
+          >
+            <p>Print</p>{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0 1 10.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0 .229 2.523a1.125 1.125 0 0 1-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0 0 21 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 0 0-1.913-.247M6.34 18H5.25A2.25 2.25 0 0 1 3 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 0 1 1.913-.247m10.5 0a48.536 48.536 0 0 0-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5Zm-3 0h.008v.008H15V10.5Z"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
 
       <h3 className="my-10 text-xl font-bold print:break-before-page">
         {result ?? "..."}
