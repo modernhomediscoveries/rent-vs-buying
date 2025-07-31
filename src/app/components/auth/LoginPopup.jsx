aa88f7d1c10e1d54d9a8824b5a45c900f2b4d21a
EstateGather <152664707+modernhomediscoveries@users.noreply.github.com>
2025-07-31 20:05:26 +0600

    Update LoginPopup.jsx

--- src/app/components/auth/LoginPopup.jsx
+++ src/app/components/auth/LoginPopup.jsx
@@ -4,7 +4,7 @@ const LoginPopup = ({ onClick }) => {
       <p className="text-xl">You must login to save reports.</p>
       <div className="flex items-center justify-between mt-10 text-center flex-wrap gap-5">
         <button
-          className="bg-gray-100 hover:bg-gray-200 transition-all px-5 py-2 rounded text-black w-28"
+          className="bg-gray-100 hover:bg-gray-200 transition-all px-5 py-2 rounded-full text-black w-28"
           onClick={onClick}
         >
           Later
@@ -12,7 +12,7 @@ const LoginPopup = ({ onClick }) => {
         <a
           href="https://estategather.com/sign-up-log-in"
           target="_parent"
-          className="bg-primary_green hover:bg-primary_green/80 transition-all text-white px-5 py-2 cursor-pointer rounded w-28"
+          className="bg-primary_green hover:bg-primary_green/80 transition-all text-white px-5 py-2 cursor-pointer rounded-full w-28"
           onClick="document.getElementById('myIframe').contentWindow.location.reload();"
         >
           Login
